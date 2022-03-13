3c523fccd65f34e69533cdf9d046b43df1e15994
Ruslan Magana Vsevolodovna <51296249+ruslanmv@users.noreply.github.com>
2022-03-13 18:40:41 +0100

    Normalizing the gamepad values
    Normalizing the gamepad values

--- versions/0.01/getgamepad.py
+++ versions/0.01/getgamepad.py
@@ -1,7 +1,17 @@
 from readPad import *
+# Create a function that normalize "Axis are -32768 to 32767"
+#Lx', 'Ly', 'Rx', 'Ry'
+def normalize(x): return round(x / 32768,1)  
+#'LT', 'RT'
+def normalizet(x): return round(x / 255,1)  
 def gamepad_check():
     con = rPad(1)
     #print(f'State:{con.read}')
     dictionary=con.read
     lista=list(dictionary.values())
+    # Create a list that applies updated() to all elements of lista
+    listab=list(map(normalize, lista[2:6]))
+    lista[2:6] = listab[:]
+    listac=list(map(normalizet, lista[0:2]))
+    lista[0:2] = listac[:]
     return lista
